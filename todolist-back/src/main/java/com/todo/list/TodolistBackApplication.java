db32d73390ca328e2c1835c3e99585a4b35925c5
brostone45 <dilanale38@gmail.com>
2024-08-20 21:49:44 -0500

    Add initial structure back

---
+++ todolist-back/src/main/java/com/todo/list/TodolistBackApplication.java
@@ -0,0 +1,13 @@
+package com.todo.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistBackApplication.class, args);
+	}
+
+}
