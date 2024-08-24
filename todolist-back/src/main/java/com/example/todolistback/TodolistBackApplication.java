31d6281523eb8caaad14685fdef74005378c75b0
brostone45 <dilanale38@gmail.com>
2024-08-23 19:00:19 -0500

    Fix initial structure back Maven

---
+++ todolist-back/src/main/java/com/example/todolistback/TodolistBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolistback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodolistBackApplication.class, args);
+    }
+
+}
---
+++ todolist-back/src/test/java/com/example/todolistback/TodolistBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todolistback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
