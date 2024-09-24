319e32cc03c154dcc7cf9d5ac97731f1a6fd4ccb
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-09-23 20:49:06 -0500

    Add input component and styles

---
+++ todolist-front/src/modules/shared/components/input/input.js
@@ -0,0 +1,31 @@
+import styles from './input.module.css'
+
+export function generateInputComponent({ labelText, placeholder, type }) {
+  const container = document.createElement('div')
+  container.className = styles['input-container']
+
+  const label = generateLabelElement({ labelText })
+  const input = generateInputElement({ placeholder, type })
+
+  container.appendChild(label)
+  container.appendChild(input)
+
+  return container
+}
+
+function generateLabelElement({ labelText }) {
+  const label = document.createElement('label')
+  label.className = 'input-label'
+  label.textContent = labelText
+
+  return label
+}
+
+function generateInputElement({ placeholder, type }) {
+  const input = document.createElement('input')
+  input.className = 'input'
+  input.placeholder = placeholder
+  input.type = type
+
+  return input
+}
