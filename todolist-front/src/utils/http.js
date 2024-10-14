72e28616532bc95f5a219579093c85c4d7cb5f32
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-10-13 21:12:20 -0500

    feat: Implement POST request utility function for API interactions

---
+++ todolist-front/src/utils/http.js
@@ -0,0 +1,11 @@
+export async function post({url, data}) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+
+  return response
+}
