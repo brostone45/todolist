161b637a15e783b8922a8c05e1ce16c538a95b3e
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-11-10 22:35:48 -0500

    Refactor home page to use header component

--- todolist-front/src/modules/home/pages/Home.js
+++ todolist-front/src/modules/home/pages/Home.js
@@ -1,17 +1,7 @@
-import styles from './home.module.css'
+import { Header } from '../components/header/header'
 
 export function Home({ app }) {
-  const header = document.createElement('header')
-  header.innerHTML = `
-    <div>
-      <h2>Hey, Steve</h2>
-      <p>Wednesday, November 9</p>
-    </div>
-
-    <picture>
-      <img src="https://yesiddn.me/assets/img/profile.jpg" alt="Profile picture" />
-    </picture>
-  `
+  const header = Header()
 
   app.innerHTML = ''
   app.append(header)
