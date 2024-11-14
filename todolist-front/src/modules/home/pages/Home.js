837a2698e979ba344fc8460d20ce618d2ad1a95e
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-11-13 23:05:52 -0500

    feat: Integrate week calendar component into home page

--- todolist-front/src/modules/home/pages/Home.js
+++ todolist-front/src/modules/home/pages/Home.js
@@ -1,8 +1,10 @@
 import { Header } from '../components/header/header'
+import { weekCalendar } from '../components/week-calendar/week-calendar'
 
 export function Home({ app }) {
   const header = Header()
+  const weekSection = weekCalendar()
 
   app.innerHTML = ''
-  app.append(header)
+  app.append(header, weekSection)
 }
