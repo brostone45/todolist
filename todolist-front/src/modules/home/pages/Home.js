dbb04c27e37a4d116ff57d15f4596bcdc262eaff
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-11-25 21:40:52 -0500

    feat: Add today's tasks section to home page layout

--- todolist-front/src/modules/home/pages/Home.js
+++ todolist-front/src/modules/home/pages/Home.js
@@ -1,10 +1,20 @@
+import styles from './home.module.css'
 import { Header } from '../components/header/header'
 import { weekCalendar } from '../components/week-calendar/week-calendar'
 
 export function Home({ app }) {
   const header = Header()
+  const main = document.createElement('main')
   const weekSection = weekCalendar()
 
+  const daystasksTitle = document.createElement('section')
+  daystasksTitle.classList.add(styles['days-tasks'])
+  daystasksTitle.innerHTML = `
+    <h2>Today's tasks</h2>
+    <button class="${styles['add-task']}">Add task</button>
+  `
+
+  main.append(weekSection, daystasksTitle)
   app.innerHTML = ''
-  app.append(header, weekSection)
+  app.append(header, main)
 }
